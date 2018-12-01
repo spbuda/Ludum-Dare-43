56fa5176110119b58f7f8c1b7c46a7e8122d67f8
Grin <xinej4@gmail.com>
2018-12-01 11:17:27 -0800

    Door Stuff and Spike Trap

--- Ludum Dare 43/Assets/OpenDoor.cs
+++ Ludum Dare 43/Assets/OpenDoor.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void onHit() {
+		transform.position = new Vector3 (transform.position.x, transform.position.y, -10);
+	}
 }
