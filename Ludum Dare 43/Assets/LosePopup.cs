3cdac316163f3602e8fe4b5804e07428f8be2ea3
Spencer Buda <budaspencer@gmail.com>
2018-11-30 20:22:33 -0700

    Added death popup

---
+++ Ludum Dare 43/Assets/LosePopup.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LosePopup : MonoBehaviour {
+	public void KillMe() {
+		Destroy (this);
+	}
+}
--- Ludum Dare 43/Assets/MainActions.cs
+++ Ludum Dare 43/Assets/MainActions.cs
@@ -5,7 +5,16 @@ using UnityEngine.SceneManagement;
 
 [CreateAssetMenu (menuName = "Scriptables/Main Actions")]
 public class MainActions : ScriptableObject {
+	public GameObject LosePopup;
 	public void StartGame() {
 		SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+	}
+
+	public void LoseGame() {
+		Instantiate (LosePopup, Vector3.zero, Quaternion.identity);
+	}
+
+	public void QuitGame() {
+		SceneManager.LoadScene ("MenuScene", LoadSceneMode.Single);
 	}
 }
