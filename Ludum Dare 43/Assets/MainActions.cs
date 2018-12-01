658922936f39ace43c3c96840735adc96b0fbac1
Spencer Buda <budaspencer@gmail.com>
2018-11-30 19:58:49 -0700

    Game Base

---
+++ Ludum Dare 43/Assets/MainActions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu (menuName = "Scriptables/Main Actions")]
+public class MainActions : ScriptableObject {
+	public void StartGame() {
+		SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+	}
+}
