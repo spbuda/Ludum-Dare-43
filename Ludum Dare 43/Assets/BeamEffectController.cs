4de23acadb3dea881cad685372fc9d49e29bd59b
Spencer Buda <budaspencer@gmail.com>
2018-12-01 11:28:49 -0700

    Fixed weirdness

--- Ludum Dare 43/Assets/BeamEffectController.cs
+++ Ludum Dare 43/Assets/BeamEffectController.cs
@@ -1,23 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class BeamEffectController : MonoBehaviour
-{
-
-	public Camera mainCamera;
-	public GameObject originPoint;
+public class BeamEffectController : MonoBehaviour {
 	public LineRenderer lineRenderer;
+	GameObject[] chillins;
 
-	Vector3 hitPoint;
+	bool beamOn = true;
+	public void Awake() {
+		chillins = GetComponentsInChildren<Transform> ().Select (t => t.gameObject).ToArray ();
+		ToggleChildrenState (false);
+	}
 
-	void Update()
-    {
-		//Line is rendered from a point at index 0 to a point at index 1, in worldspace
-		lineRenderer.SetPosition (0, originPoint.transform.position); //There's an origin point marker empty gameobject on the player prefab. Use this as the point to fire the beam from
-		hitPoint = mainCamera.ScreenToWorldPoint (Input.mousePosition); //This needs to get the point of collision, or the end of the laser raycast (if it doesn't hit anything) instead of the mouse position - mouse pos is filler
-		lineRenderer.SetPosition (1, hitPoint);
+	//Vector3 hitPoint;
+	public void MakeBeam() {
+		ToggleChildrenState (true);
+	}
 
-		//TODO: if the beam is colliding with something, move the beam impact effect to the point of contact. Spark effect is intended to splatter back towards the player.
-    }
+	public void StopBeam() {
+		ToggleChildrenState (false);
+	}
+
+	public void UpdateBeam(Vector2 origin, Vector2 target) {
+		if (beamOn) {
+			//Line is rendered from a point at index 0 to a point at index 1, in worldspace
+			lineRenderer.SetPosition (0, origin); //There's an origin point marker empty gameobject on the player prefab. Use this as the point to fire the beam from
+												  //hitPoint = mainCamera.ScreenToWorldPoint (Input.mousePosition); //This needs to get the point of collision, or the end of the laser raycast (if it doesn't hit anything) instead of the mouse position - mouse pos is filler
+			lineRenderer.SetPosition (1, target);
+		}
+	}
+
+	private void ToggleChildrenState(bool state) {
+		if (state != beamOn) {
+			foreach (GameObject c in chillins) {
+				c.SetActive (state);
+			}
+			beamOn = state;
+		}
+	}
 }
--- Ludum Dare 43/Assets/ButtonLight.cs
+++ Ludum Dare 43/Assets/ButtonLight.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonLight : MonoBehaviour
-{
+public class ButtonLight : MonoBehaviour {
 	UnityEngine.Light butLight;
 
-    // Start is called before the first frame update
-    void Start(){
-		butLight = GetComponent<Light>();
+	// Start is called before the first frame update
+	void Start() {
+		butLight = GetComponent<Light> ();
 	}
 
 	public void onHit() {
