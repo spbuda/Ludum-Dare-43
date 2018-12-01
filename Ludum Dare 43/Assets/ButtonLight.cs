894ab4e96f0ecf8d86258094dd38fb577edbde01
Grin <xinej4@gmail.com>
2018-12-01 10:09:37 -0800

    OnHit scripting and button1

---
+++ Ludum Dare 43/Assets/ButtonLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLight : MonoBehaviour
+{
+	UnityEngine.Light butLight;
+
+    // Start is called before the first frame update
+    void Start(){
+		butLight = GetComponent<Light>();
+	}
+
+	public void onHit() {
+		butLight.color = Color.green;
+	}
+}
