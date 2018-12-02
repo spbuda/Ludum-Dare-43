b90ded667dae621fba49ecf69a059728a4b8f668
Grin <xinej4@gmail.com>
2018-12-01 20:45:34 -0800

    Created level scenes, destructible wall

---
+++ Ludum Dare 43/Assets/WallHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallHealth : MonoBehaviour
+{
+	public float Energy = 1f;
+	public GameObject Main;
+
+	void Update() {
+		if (Energy <= 0f) {
+			Destroy (Main);
+			return;
+		}
+	}
+	public void LoseEnergy(float amount) {
+		Energy -= amount;
+	}
+}
