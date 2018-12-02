e5f60fc9473d6db0709ebc81dd550fc86bd5d5cb
Spencer Buda <budaspencer@gmail.com>
2018-12-02 15:19:06 -0700

    Fixed dead player stuff

--- Ludum Dare 43/Assets/FinalPortal.cs
+++ Ludum Dare 43/Assets/FinalPortal.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FinalPortal : MonoBehaviour {
 	private void OnTriggerEnter2D(Collider2D collision) {
 		PlayerController player = collision.GetComponent<PlayerController> ();
-		if (player != null) {
+		if (player != null && !player.Dead) {
 			MainActions.Instance.WinGame (player.Energy);
 			Destroy (player.gameObject);
 		}
