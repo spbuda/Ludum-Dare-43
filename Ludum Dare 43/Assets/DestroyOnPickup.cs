a9eff3f66daebe5ed9f321f4b4d92ae6bb18b366
Grin <xinej4@gmail.com>
2018-12-01 16:05:39 -0800

    Health Pickups

---
+++ Ludum Dare 43/Assets/DestroyOnPickup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnPickup : MonoBehaviour
+{
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.tag == "Player") {
+			this.gameObject.SetActive(false);
+		}
+	}
+}
