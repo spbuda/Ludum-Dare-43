2214da5b1bd7266febc45977565283ba55260753
Grin <xinej4@gmail.com>
2018-12-01 19:36:17 -0800

    Finished button, worked on hp pickup

---
+++ Ludum Dare 43/Assets/PlayPickupSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tools;
+
+public class PlayPickupSound : MonoBehaviour
+{
+	public SoundEffect pickupSound;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.tag == "Player") {
+			pickupSound.Play ();
+		}
+	}
+}
---
+++ Ludum Dare 43/Assets/PushButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushButton : MonoBehaviour
+{
+	public void onHit() {
+		transform.transform.Find("ButtonOff").GetComponent<SpriteRenderer> ().enabled = false;
+		transform.transform.Find("ButtonOn").GetComponent<SpriteRenderer> ().enabled = true;
+	}
+}
