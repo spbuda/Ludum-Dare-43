8c2bd6e6d138f0dd73c80025cdab4375a4c1b485
Spencer Buda <budaspencer@gmail.com>
2018-12-02 20:14:02 -0700

    Level Menu

--- Ludum Dare 43/Assets/LosePopup.cs
+++ Ludum Dare 43/Assets/LosePopup.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LosePopup : MonoBehaviour {
-	public void ChangeScore(float value) {
-		GetComponentInChildren<ScoreValue> ().GetComponent<UnityEngine.UI.Text>().text = "" + (Mathf.RoundToInt(value * 100));
+	public delegate void NextLevel();
+
+	public NextLevel NextLevelCallback;
+	public void NextLevelClick() {
+		NextLevelCallback ();
+	}
+
+	public void ChangeScore(float value, float mult) {
+		ScoreValue[] vals = GetComponentsInChildren<ScoreValue> ();
+		vals[0].GetComponent<UnityEngine.UI.Text>().text = "" + (Mathf.RoundToInt(value * 100));
+		vals[1].GetComponent<UnityEngine.UI.Text> ().text = Mathf.RoundToInt (mult) + "x";
 	}
 	public void KillMe() {
 		Destroy (this.gameObject);
