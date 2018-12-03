9404ea87ccb5d5d59f643bce091530f3cadbfd81
Spencer Buda <budaspencer@gmail.com>
2018-12-03 15:19:35 -0700

    Added level name

---
+++ Ludum Dare 43/Assets/LevelName.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelName : MonoBehaviour {
+	// Start is called before the first frame update
+	void Start() {
+		string name = MainActions.Instance.CurrentSceneName ();
+		GetComponent<UnityEngine.UI.Text> ().text = name;
+	}
+}
