ebf17da330b0da974f8abffe0da12b54f8962ada
Grin <xinej4@gmail.com>
2018-12-03 12:47:59 -0800

    Added level WeLiveInASociety

--- Ludum Dare 43/Assets/MainActions.cs
+++ Ludum Dare 43/Assets/MainActions.cs
@@ -101,7 +101,7 @@ public class MainActions : ScriptableObject {
 	}
 
 	public enum SceneName {
-		TheHorseShoe, SpeedPadIntro, ButtonTurretIntro, HealthIntro, SpikeAndDotPadIntro, TheGlove, Strafing, Helix, Edgy, Snek, Walls, ReachForTheSkies, SolarSystem, LevelFinal
+		TheHorseShoe, SpeedPadIntro, ButtonTurretIntro, HealthIntro, SpikeAndDotPadIntro, TheGlove, Strafing, Helix, Edgy, Snek, Walls, ReachForTheSkies, SolarSystem, WeLiveInASociety, LevelFinal
 	};
 
 	public PlayerController Player { get; set; } = null;
